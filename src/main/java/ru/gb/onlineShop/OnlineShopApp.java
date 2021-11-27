8e660b54055fe8c155acff81ef72087b2574e553
AlexBotya <alex_balg@bk.ru>
2021-11-27 12:50:56 +0300

    Lesson5 Spring cloud 1st commit

--- src/main/java/ru/gb/onlineShop/OnlineShopApp.java
+++ src/main/java/ru/gb/onlineShop/OnlineShopApp.java
@@ -3,8 +3,10 @@ package ru.gb.onlineShop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 //@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableConfigServer
 @SpringBootApplication
 public class OnlineShopApp {
     public static void main(String[] args) {
